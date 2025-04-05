c5f719304d3af854c2f20f1a768dcb5ee34d900b
CharlesSBL <karl.sobolewski@outlook.com>
2025-04-05 15:18:53 +0200

    Update configuration

---
+++ commands/commands.md
@@ -0,0 +1,6 @@
+```sh
+nixos-rebuild build --flake .
+nixos-rebuild switch --flake .#mySystem 
+
+sudo code --no-sandbox --user-data-dir
+```
--- run.sh
+++
@@ -1,6 +0,0 @@
-nixos-rebuild build --flake .
-nixos-rebuild switch --flake .#mySystem 
-# Run Visual Studio Code with elevated privileges, disabling the sandbox and specifying a user data directory
-sudo code --no-sandbox --user-data-dir
-
-git add . && git commit -m "Update configuration"
