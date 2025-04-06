3801266f075fc35b0499a72bfb199f423760602f
CharlesSBL <karl.sobolewski@outlook.com>
2025-04-07 00:10:07 +0200

    Update configuration

--- nixos/modules/apps/bash-setup.nix
+++ nixos/modules/apps/bash-setup.nix
@@ -5,6 +5,10 @@
   environment.etc."bashrc-custom".text = ''
     alias nixb='cd ~/1-Nixos-Flake && sudo nixos-rebuild switch --flake .#nixrach'
     alias nixg='cd ~/1-Nixos-Flake/ && sh ./commands/git-up.sh'
+    alias ns='nix-shell -p'
+
+    alias nixc='nix-collect-garbage'
+    alias nixclean='sudo nix-collect-garbage -d && sudo nix-store --optimise'
 
     # Display manager control
     alias start-gui='sudo systemctl start gdm.service'
